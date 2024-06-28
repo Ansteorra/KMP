6bf5bdb994ad4fd1ce68c2259db85fdc7994bcfc
Josh <josh@liveoak.ws>
2024-06-28 12:41:17 +0000

    updating seed generator for plugin table locations

--- update_seed_data.sh
+++ update_seed_data.sh
@@ -1,14 +1,17 @@
 #! /bin/sh.
 
 cd ./app
-bin/cake bake seed --data AppSettings
-bin/cake bake seed --data Branches
-bin/cake bake seed --data MemberRoles
-bin/cake bake seed --data Members
-bin/cake bake seed --data RolesPermissions
-bin/cake bake seed --data Roles
-bin/cake bake seed --data Permissions
-bin/cake bake seed --data Activities.ActivityGroups
-bin/cake bake seed --data Activities.Activities
-bin/cake bake seed --data Officers.Departments
-bin/cake bake seed --data Officers.Offices
+bin/cake bake seed --data app_settings
+bin/cake bake seed --data branches
+bin/cake bake seed --data member_roles
+bin/cake bake seed --data members
+bin/cake bake seed --data roles_permissions
+bin/cake bake seed --data roles
+bin/cake bake seed --data permissions
+bin/cake bake seed --data activities_activity_groups
+bin/cake bake seed --data activities_activities
+bin/cake bake seed --data officers_departments
+bin/cake bake seed --data officers_offices
+bin/cake bake seed --data awards_domains
+bin/cake bake seed --data awards_levels
+bin/cake bake seed --data awards_awards
