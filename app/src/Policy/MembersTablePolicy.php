0d3958ed276ec08c6525791a59beefae3e160533
Josh <josh@liveoak.ws>
2024-10-01 15:02:31 +0000

    Add view members permission to core so that roles who need just view can just view (#185)

--- app/src/Policy/MembersTablePolicy.php
+++ app/src/Policy/MembersTablePolicy.php
@@ -13,6 +13,31 @@ use Authorization\IdentityInterface;
 class MembersTablePolicy extends BasePolicy
 {
     protected string $REQUIRED_PERMISSION = "Can Manage Members";
+    protected string $REQUIRED_VIEW_PERMISSION = "Can View Members";
+
+    public function scopeIndex(IdentityInterface $user, $query)
+    {
+        if ($this->_hasNamedPermission($user, $this->REQUIRED_PERMISSION)) {
+            return $query;
+        }
+        if ($this->_hasNamedPermission($user, $this->REQUIRED_VIEW_PERMISSION)) {
+            return $query;
+        }
+        return $query->where(["Members.id" => -1]);
+    }
+
+    public function canIndex(IdentityInterface $user, $entity)
+    {
+        $canDo = $this->_hasNamedPermission($user, $this->REQUIRED_PERMISSION);
+        if ($canDo) {
+            return true;
+        }
+        $canDo = $this->_hasNamedPermission($user, $this->REQUIRED_VIEW_PERMISSION);
+        if ($canDo) {
+            return true;
+        }
+        return false;
+    }
 
     public function scopeVerifyQueue(IdentityInterface $user, $query)
     {
