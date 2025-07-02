994a6a44814eab58dc1a355e5ec6262fa31b4bdf
Jean Paul de Sens <jeanpauldesens@gmail.com>
2025-07-02 23:40:08 +0000

    Add Checkall button and fixed one typo

--- app/plugins/Awards/Assets/js/controllers/rec-table-controller.js
+++ app/plugins/Awards/Assets/js/controllers/rec-table-controller.js
@@ -2,15 +2,19 @@
 
 import { Controller } from "@hotwired/stimulus"
 
+
+
 class AwardsRecommendationTable extends Controller {
     static targets = [
         "rowCheckbox",
+        "CheckAllBox"
     ];
 
     static outlets = ["outlet-btn"];
 
     checked(event) {
-
+        console.log("Check button checked ", this.element);
+        // debugger;
         let idList = [];
         this.outletBtnOutlet.btnDataValue = {};
         this.rowCheckboxTargets.forEach(input => {
@@ -26,7 +30,32 @@ class AwardsRecommendationTable extends Controller {
     connect() {
     }
 
+    checkAll(ele) {
+        console.log("Hello, Check All!", this.element);
+        // debugger;
+
+        if (this.CheckAllBoxTarget.checked) {
+            let idList = [];
+
+            for (var i = 0; i < this.rowCheckboxTargets.length; i++) {
+                this.rowCheckboxTargets[i].checked = true; // Check all checkboxes
+                idList.push(this.rowCheckboxTargets[i].value);
+
+            }
+            this.outletBtnOutlet.btnDataValue = { "ids": idList };
+
+        }
+        else {
+            this.outletBtnOutlet.btnDataValue = {};
+            for (var i = 0; i < this.rowCheckboxTargets.length; i++) {
+                this.rowCheckboxTargets[i].checked = false; // Check all checkboxes
+            }
+            this.outletBtnOutlet.btnDataValue = {};
+
+        }
+    }
 }
+
 // add to window.Controllers with a name of the controller
 if (!window.Controllers) {
     window.Controllers = {};
